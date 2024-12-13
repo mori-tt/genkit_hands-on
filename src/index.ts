9ae70c99a68c74809d21baf098f61c46f8af4ab3
T_Mori <morit0301@gmail.com>
2024-12-13 19:41:37 +0900

    Hello Genkit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { genkit, z } from "genkit";
+import { googleAI, gemini15Flash } from "@genkit-ai/googleai";
+import { logger } from "genkit/logging";
+logger.setLogLevel("debug");
+
+const ai = genkit({
+  plugins: [googleAI()],
+  model: gemini15Flash,
+});
+
+const mainFlow = ai.defineFlow(
+  {
+    name: "mainFlow",
+    inputSchema: z.string(),
+  },
+  async (input) => {
+    const { text } = await ai.generate(input);
+    return text;
+  }
+);
+
+ai.startFlowServer({ flows: [mainFlow] });
